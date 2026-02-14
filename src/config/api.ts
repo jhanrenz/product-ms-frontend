72763b577cf21bb32c9fad145d509113cea90651
Jhan Renz <jhanrenzventura@gmail.com>
2026-02-14 19:40:05 +0800

    update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'http://127.0.0.1:8000/api'
-
+const BASE_URL = import.meta.env.BASE_API_URL;
+//
 export const api = axios.create({
     baseURL: BASE_URL,
     headers : {
